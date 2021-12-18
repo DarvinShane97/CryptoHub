6f53e6c931f1171833cb237108cdba27a339d5a5
Darvin Shane <darvin.balamana@gmail.com>
2021-12-19 00:03:17 +0530

    Create Navbar and other main compoents under the menu

--- cryptohub/src/App.js
+++ cryptohub/src/App.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { Switch, Route, Link, Routes} from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import {Navbar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News} from './components';
+import './App.css';
 
 const App = () => {
     return (
-        <div>
-            <h1>Cryptohub</h1>
+        <div className='app'>
+            <div className='navbar'>
+                <Navbar />
+            </div>
+            <div className='main'>
+                <Layout>
+                    <div className='routes'>
+                        <Switch>
+                            <Route exact path="/">
+                                <HomePage />
+                            </Route>
+                            <Route exact path="/exchanges">
+                                <Exchanges />
+                            </Route>
+                            <Route exact path="/cryptocurrencies">
+                                <Cryptocurrencies />
+                            </Route>
+                            <Route exact path="/crypto/:coinId">
+                                <CryptoDetails />
+                            </Route>
+                            <Route exact path="/news">
+                                <News />
+                            </Route>
+                        </Switch>
+                    </div>
+                </Layout>
+            </div>
+            <div className='footer'>
+
+            </div>
         </div>
     )
 }
