68c23c2e092d997c3890512cc9ba0de788c69b07
Darvin Shane <darvin.balamana@gmail.com>
2021-12-23 00:05:40 +0530

    Implement the Crypto Details page update other Components

--- cryptohub/src/components/HomePage.jsx
+++ cryptohub/src/components/HomePage.jsx
@@ -28,14 +28,14 @@ const HomePage = () => {
         </Row>
         <div className='home-heading-container'>
             <Title level={2} className='home-title'>Top 12 Cryptocurrencies in the world</Title>
-            <Title level={3} className='show-more'><Link to="/cryptocurrencies">Show More</Link></Title>
+            <Title level={5} className='show-more'><Link to="/cryptocurrencies">Show More</Link></Title>
         </div>
         <Cryptocurrencies simplified/>
         <div className='home-heading-container'>
             <Title level={2} className='home-title'>Latest Crypto News</Title>
-            <Title level={3} className='show-more'><Link to="/news">Show More</Link></Title>
+            <Title level={5} className='show-more'><Link to="/news">Show More</Link></Title>
         </div>
-        <News simplifies />
+        <News simplified />
         </>
     ) 
 }
---
+++ cryptohub/src/sevices/cryptoNewsApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+        'x-bingapis-sdk': 'true',
+        'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+        'x-rapidapi-key': '27414f2ecamsh8b8f8f59c86391cp152ce1jsn9df4b7599361'
+}
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoNewsHeaders })
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        })
+    })
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
