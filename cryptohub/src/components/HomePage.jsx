67d14f7656fd6fa4c16049a45e6d5acae112d6c2
Darvin Shane <darvin.balamana@gmail.com>
2021-12-22 11:09:44 +0530

    Implement the Cryptocurrencies Page and update Home Page

--- cryptohub/src/components/HomePage.jsx
+++ cryptohub/src/components/HomePage.jsx
@@ -11,7 +11,7 @@ const { Title } = Typography;
 
 const HomePage = () => {
 
-    const { data, isFetching } = useGetCryptosQuery();
+    const { data, isFetching } = useGetCryptosQuery(12);
     const globalStats = data?.data?.stats; 
 
     if(isFetching) return 'Loading...'; 
@@ -27,10 +27,10 @@ const HomePage = () => {
             <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
         </Row>
         <div className='home-heading-container'>
-            <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Title>
+            <Title level={2} className='home-title'>Top 12 Cryptocurrencies in the world</Title>
             <Title level={3} className='show-more'><Link to="/cryptocurrencies">Show More</Link></Title>
         </div>
-        <Cryptocurrencies />
+        <Cryptocurrencies simplified/>
         <div className='home-heading-container'>
             <Title level={2} className='home-title'>Latest Crypto News</Title>
             <Title level={3} className='show-more'><Link to="/news">Show More</Link></Title>
